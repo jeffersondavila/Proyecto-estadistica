20968d3e6581fca5acf3d12708b27ebda90dc961
jeffersondavila <jeffersondavila16@gmail.com>
2020-10-26 11:10:11 -0600

    Media, Mediana y moda corregidos

--- Proyecto de Estadística/menu.cpp
+++ Proyecto de Estadística/menu.cpp
@@ -4,9 +4,8 @@
 #include <fstream>
 #include <Windows.h>
 #include <stdio.h>
-#include <math.h>
+#include <cmath>
 #include <iomanip>
-
 void gotoxy(int x, int y)
 {
 	HANDLE hcon;
@@ -18,12 +17,19 @@ void gotoxy(int x, int y)
 }
 
 using namespace std;
-
-int CantidadDatos, i, j, x, y;
-float respuesta[100], temp, n, aux, Ancho_Intervalo = 0, nMayor = 0, nMenor, sum;
-float Intervalo = 0, auxiliar, vectora[100], vectorb[100], marca_de_clase[100], FrecuenciaAcum[100], FrecuenciaRel[100], FrecuenciaRelDesc[100];
-float calculoPromedio = 0, mcMprom2, Media, x1, x2, x4;
-
+int i, j, x, y;
+float Respuesta[100];
+float Datos[100], CantidadDatos, aux;
+float nMenor, nMayor, Ancho_Intervalo, Intervalo, acumulado; int Rango;
+float vectora[100], vectorb[100], FrecuenciaAbsoluta[100], inicial = 0, sumador = 0, sumador2;
+float marca_de_clase[100];
+float FrecuenciaAbsolutaAcumulada[100], acumulador = 0;
+float FrecuenciaRel[100];
+float FrecuenciaRelDesc[100];
+float calculoPromedio = 0;
+float mcMprom2 = 0;
+float Media;
+float posicion;
 void INGRESARDATOS() {
 	int x = 30, y = 5, x1 = 90, y1 = 5;
 
@@ -35,8 +41,8 @@ void INGRESARDATOS() {
 	gotoxy(x1, 2); cout << "LISTA DE DATOS INGRESADOS:";
 	for (i = 0; i < CantidadDatos; i++)
 	{
-		gotoxy(x, y); cout << "INGRESE EL DATO # " << i + 1 << "\t"; cin >> respuesta[i];
-		gotoxy(x1, y1); cout << i + 1 << ".\t" << respuesta[i];
+		gotoxy(x, y); cout << "INGRESE EL DATO # " << i + 1 << "\t"; cin >> Datos[i];
+		gotoxy(x1, y1); cout << i + 1 << ".\t" << Datos[i];
 		gotoxy(88, y1); putchar('|');
 		gotoxy(117, y1); putchar('|');
 		y++, y1++;
@@ -48,10 +54,10 @@ void INGRESARDATOS() {
 	//CAPTURAR LOS DATOS EN RESPUESTA Y CONTARREPETICONES
 	for (i = 0; i < CantidadDatos; i++) {
 		for (j = i + 1; j < CantidadDatos; j++) {
-			if (respuesta[i] > respuesta[j]) {
-				aux = respuesta[i];
-				respuesta[i] = respuesta[j];
-				respuesta[j] = aux;
+			if (Datos[i] > Datos[j]) {
+				aux = Datos[i];
+				Datos[i] = Datos[j];
+				Datos[j] = aux;
 			}
 		}
 	}
@@ -63,7 +69,7 @@ void MODIFICARDATOS() {
 	gotoxy(x1, 2); cout << "LISTA DE DATOS INGRESADOS:";
 	for (i = 0; i < CantidadDatos; i++)
 	{
-		gotoxy(x1, y1); cout << i << ".\t" << respuesta[i];
+		gotoxy(x1, y1); cout << i << ".\t" << Datos[i];
 		y1++;
 	}
 	//imprimimos la lista de datos ya ordenados
@@ -74,18 +80,18 @@ void MODIFICARDATOS() {
 		{
 			if (i == posicion)
 			{
-				gotoxy(70, 6); cout << "INGRESE NUEVO DATO: "; cin >> respuesta[i];
-				cout << respuesta[i];
+				gotoxy(70, 6); cout << "INGRESE NUEVO DATO: "; cin >> Datos[i];
+				cout << Datos[i];
 			}
 		}
 		//se guarda la modificación y en los dos vectores
 		system("cls");
 		for (i = 0; i < CantidadDatos; i++) {
 			for (j = i + 1; j < CantidadDatos; j++) {
-				if (respuesta[i] > respuesta[j]) {
-					aux = respuesta[i];
-					respuesta[i] = respuesta[j];
-					respuesta[j] = aux;
+				if (Datos[i] > Datos[j]) {
+					aux = Datos[i];
+					Datos[i] = Datos[j];
+					Datos[j] = aux;
 				}
 			}
 		}
@@ -93,7 +99,7 @@ void MODIFICARDATOS() {
 		gotoxy(x1, 2); cout << "LA NUEVA LISTA DE DATOS INGRESADOS ES:"; x1 = 10, y1 = 5;
 		for (i = 0; i < CantidadDatos; i++)
 		{
-			gotoxy(x1, y1); cout << i << ".\t" << respuesta[i];
+			gotoxy(x1, y1); cout << i << ".\t" << Datos[i];
 			y1++;
 		}
 		//se imprime la lista del vector con los datos ya ordenados
@@ -105,41 +111,42 @@ void MODIFICARDATOS() {
 	}
 	system("pause>null");
 }
-void CONTARREPETICIONES() {
-	for (int q = 0; q < 300; q++) {
-		sum = 0;
-		for (j = 0; j < CantidadDatos; j++) {
-			if (respuesta[q] == respuesta[j]) {
-				sum = sum + 1;
-			}
-		}
-		if (0 < sum && sum < 2) {//si es mayor a 0 el contador es porque se repite el numero
-			gotoxy(x, y); cout << "El número " << respuesta[q] << " se repite " << sum << " vez.";
-			y++;
-		}
-		if (sum >= 2)
-		{
-			gotoxy(x, y); cout << "El número " << respuesta[q] << " se repite " << sum << " veces.";
-			y++;
+void NumeroMayor() {
+	x = 30;
+	for (i = 0; i < CantidadDatos; i++)
+	{
+		if (Datos[i] > nMayor) {
+			nMayor = Datos[i];
 		}
 	}
-
-	x = 25, y = 5;
-	gotoxy(x, 5); cout << "LISTADO DE DATOS ORDENADOS: ";
-	x = 55;
-	for (i = 0; i < CantidadDatos; i++) {
-		gotoxy(x, y); cout << respuesta[i] << ",";
-		x = x + 7;
-	}
-	//se imprime la lista con los datos ordenados
-	system("pause>null");
+	gotoxy(x, 5); cout << "EL NUMERO MAYOR ES: " << nMayor;
 }
-void FRECUENCIAABSOLUTA() {
-	int y = 10;
-	float inicial = 0, sumador = 0, sumador2;
+void NumeroMenor() {
+	nMenor = nMayor;
+	for (i = 0; i < CantidadDatos; i++)
+	{
+		if (Datos[i] < nMenor) {
+			nMenor = Datos[i];
+		}
+	}
+	gotoxy(60, 5); cout << "EL NUMERO MENOR ES: " << nMenor;
+}
+void RANGO() {
+	Rango = nMayor - nMenor;
+	gotoxy(90, 5); cout << "EL RANGO ES DE: " << setprecision(2) << Rango;
+}
+void INTERVALO() {
+	Intervalo = 1 + 3.3 * log10(CantidadDatos);
+	gotoxy(30, 7); cout << "EL INTERVALO ES DE: " << fixed << setprecision(0) << Intervalo;
+}
+void ANCHO_DE_INTERVALO() {
+	Ancho_Intervalo = Rango / Intervalo;
+	gotoxy(60, 7); cout << "Y EL ANCHO DEL INTERVALO ES DE: " << setprecision(2) << Ancho_Intervalo << "\n\n";
+}
+void IMPRIMIR_INTERVALOS() {
+	y = 10;
 	inicial = nMenor;
 	sumador = nMenor + Ancho_Intervalo;
-
 	for (int i = 0; i < Intervalo; i++)
 	{
 		gotoxy(15, 9); cout << "No. Intervalos";
@@ -151,13 +158,15 @@ void FRECUENCIAABSOLUTA() {
 		sumador = sumador + Ancho_Intervalo;
 		y++;
 	}
+}
+void FRECUENCIA_ABSOLUTA() {
 	y = 10;
 	for (int i = 0; i < Intervalo; i++)
 	{
 		sumador2 = 0;
 		for (int j = 0; j < CantidadDatos; j++)
 		{
-			if (respuesta[j] >= vectora[i] && respuesta[j] < vectorb[i])
+			if (Datos[j] >= vectora[i] && Datos[j] < vectorb[i])
 			{
 				sumador2++;
 			}
@@ -166,13 +175,13 @@ void FRECUENCIAABSOLUTA() {
 		{
 			gotoxy(40, 9); cout << "fi";
 			gotoxy(40, y); cout << sumador2 << endl;
+			FrecuenciaAbsoluta[i] = sumador2;
+			FrecuenciaRel[i] = sumador2;
 		}
-		FrecuenciaAcum[i] = sumador2;
-		FrecuenciaRel[i] = sumador2;
 		y++;
 	}
 }
-void MARCADECLASE() {
+void MARCA_DE_CLASE() {
 	int intervaloizq = 0, intervaloder = 0, y = 10;
 	for (int i = 0; i < Intervalo; i++)
 	{
@@ -182,19 +191,18 @@ void MARCADECLASE() {
 		y++;
 	}
 }
-void FRECUENCIAABSOLUTAACUMULADA() {
-	float acumulador = 0;
-	int y = 10;
-
+void FRECUENCIA_ABSOLUTA_ACUMULADA() {
+	y = 10;
 	for (i = 0; i <= Intervalo; i++)
 	{
-		acumulador = FrecuenciaAcum[i] + acumulador;
+		acumulador = FrecuenciaAbsoluta[i] + acumulador;
+		FrecuenciaAbsolutaAcumulada[i] = acumulador;
 		gotoxy(60, 9); cout << "Fi";
-		gotoxy(60, y); cout << acumulador;
+		gotoxy(60, y); cout << FrecuenciaAbsolutaAcumulada[i];
 		y++;
 	}
 }
-void FRECUENCIARELATIVA() {
+void FRECUENCIA_RELATIVA() {
 	y = 10;
 	for (i = 0; i < Intervalo; i++)
 	{
@@ -204,20 +212,23 @@ void FRECUENCIARELATIVA() {
 		y++;
 	}
 }
-void FRECUENCIAACUMULADA_DESCENDENTE() {
-	float acumulador = CantidadDatos;
+void FRECUENCIA_ACUMULADA_DESCENDENTE() {
+	float auxi = 0;
+	acumulador = CantidadDatos;
 	y = 10;
 	for (i = 0; i <= Intervalo; i++)
 	{
 		gotoxy(80, 9); cout << "fi desc.";
 		gotoxy(80, y); cout << acumulador;
-		acumulador = acumulador - FrecuenciaAcum[i];
+		auxi = auxi + acumulador;
+		acumulador = acumulador - FrecuenciaAbsoluta[i];
 		y++;
 	}
+	gotoxy(80, y); cout << "--------"; y++;
+	gotoxy(80, y); cout << auxi;
 }
-void FRECUENCIARELATIVAASCENDENTE() {
-	float acumulador;
-	int y = 10;
+void FRECUENCIA_RELATIVA_ASCENDENTE() {
+	y = 10;
 	acumulador = FrecuenciaRelDesc[0];
 	for (i = 1; i < Intervalo + 1; i++)
 	{
@@ -227,8 +238,8 @@ void FRECUENCIARELATIVAASCENDENTE() {
 		y++;
 	}
 }
-void FRECUENCIARELATIVADESCENDENTE() {
-	float acumulador = 1;
+void FRECUENCIA_RELATIVA_DESCENDENTE() {
+	acumulador = 1;
 	y = 10;
 	for (i = 0; i <= Intervalo; i++)
 	{
@@ -243,34 +254,35 @@ void XIFI() {
 	for (int i = 0; i < Intervalo; i++)
 	{
 		gotoxy(110, 9); cout << "fi*xi";
-		calculoPromedio = FrecuenciaAcum[i] * marca_de_clase[i];
+		calculoPromedio = FrecuenciaAbsoluta[i] * marca_de_clase[i];
 		gotoxy(110, y); cout << calculoPromedio << endl;
 		y++;
 	}
 }
 void PROMEDIO() {
-	int y = 10;
 	float mcMprom = 0;
 	for (int i = 0; i < Intervalo; i++)
 	{
-		calculoPromedio = FrecuenciaAcum[i] * marca_de_clase[i];
+		calculoPromedio = FrecuenciaAbsoluta[i] * marca_de_clase[i];
 		mcMprom = mcMprom + calculoPromedio;
 	}
 	mcMprom2 = mcMprom / CantidadDatos;
 	cout << "\nX = " << mcMprom << " / " << CantidadDatos << " = " << mcMprom2;
 }
 void XmenosPROMEDIO() {
-	int y = 10;
+	float x1;
+	y = 10;
 	for (int i = 0; i < Intervalo; i++)
 	{
 		gotoxy(120, 9); cout << "|Xi - X|";
-		x1 = (marca_de_clase[i] - mcMprom2) * -1;
+		x1 = abs(marca_de_clase[i] - mcMprom2);
 		gotoxy(120, y); cout << x1 << endl;
 		y++;
 	}
 }
 void XmPCUADRADO() {
-	int y = 10;
+	float x2;
+	y = 10;
 	for (int i = 0; i < Intervalo; i++)
 	{
 		gotoxy(130, 9); cout << "|Xi - X|^2";
@@ -280,89 +292,163 @@ void XmPCUADRADO() {
 	}
 }
 void XmPCF() {
-	int y = 10;
+	float x3;
+	y = 10;
 	for (int i = 0; i < Intervalo; i++)
 	{
 		gotoxy(143, 9); cout << "|Xi - X|^2 * fi";
-		x4 = ((marca_de_clase[i] - mcMprom2) * (marca_de_clase[i] - mcMprom2)) * FrecuenciaAcum[i];
-		gotoxy(145, y); cout << x4 << endl;
+		x3 = ((marca_de_clase[i] - mcMprom2) * (marca_de_clase[i] - mcMprom2)) * FrecuenciaAbsoluta[i];
+		gotoxy(145, y); cout << x3 << endl;
 		y++;
 	}
 }
-void MEDIA() {
-	int y = 10;
-	float mcMprom = 0;
-	for (int i = 0; i < Intervalo; i++)
-	{
-		calculoPromedio = FrecuenciaAcum[i] * marca_de_clase[i];
-		mcMprom = mcMprom + calculoPromedio;
-	}
-	Media = mcMprom / CantidadDatos;
-	cout << "\nX = " << mcMprom << " / " << CantidadDatos << " = " << Media;
-}
-void MEDIANA() {
-	float posicion;
-	posicion = CantidadDatos / 2;
-	cout << CantidadDatos << " / 2 = " << posicion;
-}
 void DISTRIBUCION_DE_FRECUENCIAS() {
-	float acumulado = 0;
-	int Rango = 0, x = 5;
-
-	//NUMERO MAYOR
-	for (i = 0; i < CantidadDatos; i++)
-	{
-		if (respuesta[i] > nMayor) {
-			nMayor = respuesta[i];
-		}
-	}
-	gotoxy(x, 5); cout << "EL NUMERO MAYOR ES: " << nMayor;
-	//NUMERO MENOR
-	nMenor = nMayor;
-	for (i = 0; i < CantidadDatos; i++)
-	{
-		if (respuesta[i] < nMenor) {
-			nMenor = respuesta[i];
-		}
-	}
-	gotoxy(30, 5); cout << "EL NUMERO MENOR ES: " << nMenor;
-	Rango = nMayor - nMenor;
-
-	//RANGO
-	gotoxy(57, 5); cout << "EL RANGO ES DE: " << Rango;
-
-	//INTERVALO
-	Intervalo = 1 + 3.3 * log10(CantidadDatos);
-	gotoxy(x, 7); cout << "EL INTERVALO ES DE: " << fixed << setprecision(0) << Intervalo;
-
-	//ANCHO DEL INTERVALO
-	Ancho_Intervalo = Rango / Intervalo;
-	gotoxy(35, 7); cout << "Y EL ANCHO DEL INTERVALO ES DE: " << Ancho_Intervalo << "\n\n";
-
-	////////////////////TABLA////////////////////
-	FRECUENCIAABSOLUTA();
-	MARCADECLASE();
-	FRECUENCIAABSOLUTAACUMULADA();
-	FRECUENCIARELATIVA();
-	FRECUENCIAACUMULADA_DESCENDENTE();
-	FRECUENCIARELATIVAASCENDENTE();
-	FRECUENCIARELATIVADESCENDENTE();
+	NumeroMayor();
+	NumeroMenor();
+	RANGO();
+	INTERVALO();
+	ANCHO_DE_INTERVALO();
+	IMPRIMIR_INTERVALOS();
+	FRECUENCIA_ABSOLUTA();
+	MARCA_DE_CLASE();
+	FRECUENCIA_ABSOLUTA_ACUMULADA();
+	FRECUENCIA_RELATIVA();
+	FRECUENCIA_ACUMULADA_DESCENDENTE();
+	FRECUENCIA_RELATIVA_ASCENDENTE();
+	FRECUENCIA_RELATIVA_DESCENDENTE();
 	XIFI();
 	PROMEDIO();
 	XmenosPROMEDIO();
 	XmPCUADRADO();
 	XmPCF();
-
 	system("pause>null");
 };
-void MEDIDAS_DE_TENDENCIA_CENTRAL() {
-	cout << "MEDIDAS DE TENDENCIA CENTRAL" << endl;
-	cout << "\nMEDIA (PROMEDIO)" << endl;
-	MEDIA();
-	cout << "\nMEDIANA" << endl;
-	MEDIANA();
-
-	system("pause>null");
+void MEDIA() {
+	y = 10;
+	float mcMprom = 0;
+	for (int i = 0; i < Intervalo; i++)
+	{
+		calculoPromedio = FrecuenciaAbsoluta[i] * marca_de_clase[i];
+		mcMprom = mcMprom + calculoPromedio;
+	}
+	Media = mcMprom / CantidadDatos;
+	cout << "\n\n----------------------";
+	cout << "\nX = " << mcMprom << " / " << CantidadDatos << " = " << Media;
+	cout << "\n----------------------\n";
+}
+void MEDIANA() {
+	float calculo1, calculo2, calculo3, totalMediana, auxi[100], encontrado2 = 0;
+	bool encontrado = false;
+	aux = 650;
+	posicion = CantidadDatos / 2;
+	cout << "\n\nPosicion = " << CantidadDatos << " / 2 = " << posicion << endl;
+	for (int i = 0; i < Intervalo; i++)
+	{
+		if (posicion == FrecuenciaAbsolutaAcumulada[i])
+		{
+			cout << "\n\n**********MEDIANA**********\n\n";
+			cout << " FRECUENCIA ABSOLUTA: " << FrecuenciaAbsoluta[i] << endl;
+			cout << " FRECUENCIA ABSOLUTA ACUMULADA - 1: " << FrecuenciaAbsolutaAcumulada[i - 1] << endl;
+			cout << " LIMITE INFERIOR: " << vectora[i] << endl;
+			cout << " POSICIÓN: " << posicion << endl;
+			cout << " ANCHO DE INTERVALO: " << Ancho_Intervalo << endl;
+			calculo1 = posicion - FrecuenciaAbsolutaAcumulada[i - 1];
+			calculo2 = calculo1 / FrecuenciaAbsoluta[i];
+			calculo3 = calculo2 * Ancho_Intervalo;
+			totalMediana = vectora[i] + calculo3;
+			cout << " MEDIANA: " << totalMediana << endl;
+			encontrado = true;
+			encontrado = 1;
+		}
+		if (FrecuenciaAbsolutaAcumulada[i] > posicion)
+		{
+			if (aux > FrecuenciaAbsolutaAcumulada[i])
+			{
+				aux = FrecuenciaAbsolutaAcumulada[i];
+			}
+		}
+	}
+	if (encontrado == false && encontrado2 == 0)
+	{
+		float acum = 0;
+		acum = aux - posicion;
+		cout << " A la posicion " << posicion << " Se le suma " << acum << " para encontrar su posición en la tabla: " << aux << endl;
+		for (int i = 0; i < Intervalo; i++)
+		{
+			if (aux == FrecuenciaAbsolutaAcumulada[i])
+			{
+				cout << "\n\n**********MEDIANA**********\n\n";
+				cout << " FRECUENCIA ABSOLUTA: " << FrecuenciaAbsoluta[i] << endl;
+				cout << " FRECUENCIA ABSOLUTA ACUMULADA - 1: " << FrecuenciaAbsolutaAcumulada[i - 1] << endl;
+				cout << " LIMITE INFERIOR: " << vectora[i] << endl;
+				cout << " POSICIÓN: " << posicion << endl;
+				cout << " ANCHO DE INTERVALO: " << Ancho_Intervalo << endl;
+				calculo1 = posicion - FrecuenciaAbsolutaAcumulada[i - 1];
+				calculo2 = calculo1 / FrecuenciaAbsoluta[i];
+				calculo3 = calculo2 * Ancho_Intervalo;
+				totalMediana = vectora[i] + calculo3;
+				cout << " MEDIANA: " << totalMediana << endl;
+			}
+		}
+	}
+}
+void MODA() {
+	float calculo1, calculo2, calculo3, calculo4, totalModa, auxi[100], encontrado2 = 0;
+	bool encontrado = false;
+	aux = 650;
+	posicion = CantidadDatos / 2;
+	cout << "\n\nPosicion = " << CantidadDatos << " / 2 = " << posicion << endl;
+	for (int i = 0; i < Intervalo; i++)
+	{
+		if (posicion == FrecuenciaAbsolutaAcumulada[i])
+		{
+			cout << "\n\n**********MODA**********\n\n";
+			cout << " FRECUENCIA ABSOLUTA: " << FrecuenciaAbsoluta[i] << endl;
+			cout << " FRECUENCIA ABSOLUTA - 1: " << FrecuenciaAbsoluta[i - 1] << endl;
+			cout << " FRECUENCIA ABSOLUTA + 1: " << FrecuenciaAbsoluta[i + 1] << endl;
+			cout << " LIMITE INFERIOR: " << vectora[i] << endl;
+			cout << " ANCHO DE INTERVALO: " << Ancho_Intervalo << endl;
+			calculo1 = FrecuenciaAbsoluta[i] - FrecuenciaAbsoluta[i - 1];
+			calculo2 = (FrecuenciaAbsoluta[i] - FrecuenciaAbsoluta[i - 1]) + (FrecuenciaAbsoluta[i] - FrecuenciaAbsoluta[i + 1]);
+			calculo3 = calculo1 / calculo2;
+			calculo4 = calculo3 * Ancho_Intervalo;
+			totalModa = vectora[i] + calculo4;
+			cout << " MODA: " << totalModa << endl;
+			encontrado = true;
+			encontrado = 1;
+		}
+		if (FrecuenciaAbsolutaAcumulada[i] > posicion)
+		{
+			if (aux > FrecuenciaAbsolutaAcumulada[i])
+			{
+				aux = FrecuenciaAbsolutaAcumulada[i];
+			}
+		}
+	}
+	if (encontrado == false && encontrado2 == 0)
+	{
+		float acum = 0;
+		acum = aux - posicion;
+		cout << " A la posicion " << posicion << " Se le suma " << acum << " para encontrar su posición en la tabla: " << aux << endl;
+		for (int i = 0; i < Intervalo; i++)
+		{
+			if (aux == FrecuenciaAbsolutaAcumulada[i])
+			{
+				cout << "\n\n**********MODA**********\n\n";
+				cout << " FRECUENCIA ABSOLUTA: " << FrecuenciaAbsoluta[i] << endl;
+				cout << " FRECUENCIA ABSOLUTA - 1: " << FrecuenciaAbsoluta[i - 1] << endl;
+				cout << " FRECUENCIA ABSOLUTA + 1: " << FrecuenciaAbsoluta[i + 1] << endl;
+				cout << " LIMITE INFERIOR: " << vectora[i] << endl;
+				cout << " ANCHO DE INTERVALO: " << Ancho_Intervalo << endl;
+				calculo1 = FrecuenciaAbsoluta[i] - FrecuenciaAbsoluta[i - 1];
+				calculo2 = (FrecuenciaAbsoluta[i] - FrecuenciaAbsoluta[i - 1]) + (FrecuenciaAbsoluta[i] - FrecuenciaAbsoluta[i + 1]);
+				calculo3 = calculo1 / calculo2;
+				calculo4 = calculo3 * Ancho_Intervalo;
+				totalModa = vectora[i] + calculo4;
+				cout << " MODA: " << totalModa << endl;
+			}
+		}
+	}
 }
 void menu() {
 	int opcion = 0;
@@ -406,7 +492,6 @@ void menu() {
 			break;
 		case 3:
 			system("cls");
-			CONTARREPETICIONES();
 			break;
 		case 4:
 			system("cls");
@@ -414,7 +499,10 @@ void menu() {
 			break;
 		case 5:
 			system("cls");
-			MEDIDAS_DE_TENDENCIA_CENTRAL();
+			MEDIA();
+			MEDIANA();
+			MODA();
+			system("pause>null");
 			break;
 		default:
 			system("cls");
@@ -423,8 +511,7 @@ void menu() {
 	} while (opcion != 8);
 
 };
-int main()
-{
+int main() {
 	setlocale(LC_CTYPE, "Spanish");
 	menu();
 	system("pause>null");
